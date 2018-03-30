04c18485c6b2d1986db35db06278886983f79b8a
Asaph Mutheru <chefasaph@gmail.com>
2018-03-30 20:23:32 +0300

    Update the scripts.js file

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,38 @@
-$(document).ready(function(){
-    $("button").click(function(){
-        $("#number-slot").add(); 
+//back-end
+
+function pingPong(ansWer){
+   var thePingPongArray=[];
+   for (var i=0; i<ansWer; i +1){
+
+
+if (i%15===0){
+    thePingPongArray.push("pingpong");
+
+{
+else if(i%3===0){
+    thePingPongArray.push("ping");
+}
+
+else if(i%5===0){
+    thePingPongArray.push("pong");
+}
+
+else {
+    thePingPongArray.push(i);
+    }
+}
+return thePingPongArray
+
+//user-interface
+
+document.ready(function){
+  $("number-slot#pingpongz").submit (function(event)){
+    event.preventDefault;
+    var ansWer=parseInt($("#number-slot#number").val());
+    var PingPongArray=pingPong(ansWer);
+    $("ul#ping").empty();
+    pingPongArray.forEach(function(item){
+        $("ul#ping").append("<li>"+item+"</li>");
+     });
     });
-});
+  });
